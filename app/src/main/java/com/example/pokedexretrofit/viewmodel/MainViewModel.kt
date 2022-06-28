a66ceaed439a8fabb6065eb8c40aad886338995c
InsertyEXE <insertyend@gmail.com>
2022-06-27 22:06:39 -0300

    feat: viewmodel and factory created

---
+++ app/src/main/java/com/example/pokedexretrofit/viewmodel/MainViewModel.kt
@@ -0,0 +1,57 @@
+package com.example.pokedexretrofit.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.pokedexretrofit.model.Pokemon
+import com.example.pokedexretrofit.model.PokemonLista
+import com.example.pokedexretrofit.rest.MainRepository
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+class MainViewModel(val repository: MainRepository) : ViewModel() {
+
+    val pokemon = MutableLiveData<Pokemon>()
+    val erroServerMessage = MutableLiveData<String>()
+    val errorPokemonNaoEcontrado = MutableLiveData<String>()
+
+    fun listarPokemons() {
+
+        repository.listarPokemons(151)
+            .enqueue(object : Callback<PokemonLista> {
+                override fun onResponse(
+                    call: Call<PokemonLista>,
+                    response: Response<PokemonLista>
+                ) {
+
+                    if (!response.isSuccessful)
+                        erroServerMessage.postValue("Servidor sem resposta")
+
+                    val pokemons = response.body()?.results
+
+                    pokemons?.map {
+
+                        repository.buscarPokemon(it.name).enqueue(object : Callback<Pokemon> {
+                            override fun onResponse(
+                                call: Call<Pokemon>,
+                                response: Response<Pokemon>
+                            ) {
+                                pokemon.postValue(response.body())
+                            }
+
+                            override fun onFailure(call: Call<Pokemon>, t: Throwable) {
+                                errorPokemonNaoEcontrado.postValue("Pokemon não encontrado")
+                            }
+
+                        })
+                    }
+                }
+
+                override fun onFailure(call: Call<PokemonLista>, t: Throwable) {
+                    erroServerMessage.postValue("Servidor sem resposta")
+                }
+
+            })
+    }
+}
